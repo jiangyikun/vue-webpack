b56cad8ec9f42886f0fc01f6cb07d9f1b72d5634
jiangyikun <1430988321@qq.com>
2019-07-23 23:34:05 +0800

    0723-push

---
+++ src/app.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>{{text}}</div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      text:'asdasd'
+    }
+  }
+}
+</script>
+
+<style>
+div{
+  color:red
+}
+</style>
+
+
