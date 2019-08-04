cae7fa7a95b94b9bd62484a7ac9bd1c9bdc64100
jiangyikun <1430988321@qq.com>
2019-08-04 23:32:23 +0800

    0804-业务逻辑

--- src/todo/todo.vue
+++ src/todo/todo.vue
@@ -1,55 +1,69 @@
 <template>
   <section class="realApp">
-    <input 
-    type="text" 
-    class="addInput" 
-    autofocus='autofocus' 
-    placeholder="接下去要做什么？" 
-    @keyup.enter="addTodo">
-    <Item :todo="todo"></Item>
+    <input
+      type="text"
+      class="addInput"
+      autofocus="autofocus"
+      placeholder="接下去要做什么？"
+      @keyup.enter="addTodo"
+    />
+    <Item 
+    v-for="item in todos" 
+    :key="item.id" 
+    :todo="item"
+    @delete='deleteByid' />
     <Tabs :filter="filter"></Tabs>
   </section>
 </template>
 
 <script>
-import Item from './item.vue'
-import Tabs from './tabs.vue'
+import Item from "./item.vue";
+import Tabs from "./tabs.vue";
+let id = 0;
 export default {
-  data(){
+  data() {
     return {
-      todo:{
-        id:0,
-        content:'this is todo',
-        completed:false,
-      },
-      filter:'all'
-    }
+      todos: [],
+      filter: "all"
+    };
   },
-  components:{
+  components: {
     Item,
     Tabs
   },
-  methods:{
-    addTodo(){
-
+  methods: {
+    addTodo(e) {
+      let value = e.target.value;
+      this.todos.unshift({
+        id: id++,
+        content: value,
+        completed: false
+      });
+      e.target.value='';
+    },
+    deleteByid(id){
+      this.todos = this.todos.filter(item=>{
+        return item.id!==id
+      })
+      console.log(id);
     }
   }
-}
+};
 </script>
 
 <style lang="stylus" scoped>
-.realApp{
-  width 600px
-  margin 0 auto 
-  box-shadow 0 0 5px #666
+.realApp {
+  width: 600px;
+  margin: 0 auto;
+  box-shadow: 0 0 5px #666;
 }
-    
-.addInput{
-  position relative
-  border none
-  outline none 
-  padding 20px 0 20px 20px
-  width 580px;
-  font-size 20px
+
+.addInput {
+  position: relative;
+  border: none;
+  outline: none;
+  padding: 20px 0 20px 20px;
+  width: 580px;
+  font-size: 20px;
 }
 </style>
